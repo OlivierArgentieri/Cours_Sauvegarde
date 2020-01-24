391028999a7d6c5b7ab7b347209fbdfbfab1201f
Argentieri <olivierargentieri@gmail.com>
2020-01-24 09:44:21 +0100

    add project

---
+++ Cours_Sauvegarde/Assets/Save/Scripts/Save/SV_GameData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SV_GameData : MonoBehaviour
+{
+    #region f/p
+    [SerializeField] SV_ProfileData profileData = new SV_ProfileData();
+    
+    #endregion
+
+
+    #region unity methods
+    #endregion
+
+    #region custom methods
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
